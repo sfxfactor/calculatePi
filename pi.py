91646dff25aecbe8481f37e56be36b9ac3f42f26
Samuel Factor <sfxfactor@gmail.com>
2016-03-06 17:00:46 -0600

    add progress bar and ipnb

--- pi.py
+++ pi.py
@@ -4,11 +4,20 @@ import time
 
 n=int(sys.argv[1])
 m=int(sys.argv[2])
+N=m*n
 
 nhits=0
 
 st=time.clock()
 for i in range(m):
+    #progress bar
+    percent = float(i) / m
+    hashes = '#' * int(round(percent * 20))
+    spaces = ' ' * (20 - len(hashes))
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+    #if (i % (m/100) == 0):
+    #    print i/(m/100),'%'
 
     thetas=np.random.random_sample(size=n)*180.
     Ds=np.random.random_sample(size=n)*0.5
@@ -20,6 +29,6 @@ et=time.clock()
 pi=2.*m*n/nhits
 
 print 'pi~=',pi
-print 'calculated pi to ',np.abs((pi-np.pi)/np.pi)*100,'%'
+print 'calculated pi to ',np.abs((pi-np.pi)/np.pi)*100.,'%'
 print 'calculation took ',(et-st),' seconds.'
-print 'used ',n*m,' trials.'
+print 'used 10^',np.log10(n*m),' trials.'
